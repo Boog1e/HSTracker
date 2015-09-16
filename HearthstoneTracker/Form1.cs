302699794f17a2bf999edbafdb96ef39ba83bc68
Boog1e <mathiassoerensen2010@live.dk>
2015-09-16 19:10:52 +0200

    Reader når ændringer i tekstfil sker.

--- HearthstoneTracker/Form1.cs
+++ HearthstoneTracker/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,43 @@ namespace HearthstoneTracker
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //string path = @"D:\Program Files (x86)\Battle.net\Hearthstone\Hearthstone_Data";
+                string path = @"C:\Users\Mathias Sørensen\Desktop";
+                var watch = new FileSystemWatcher();
+                watch.Path = path;
+                //watch.Filter = "output_log.txt";
+                watch.Filter = "hstracker.txt";
+                watch.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite;
+                watch.Changed += new FileSystemEventHandler(OnChanged);
+                watch.EnableRaisingEvents = true;
+            }
+            catch
+            {}
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            try
+            {
+                string path = @"C:\Users\Mathias Sørensen\Desktop\hstracker.txt";
+                //@"D:\Program Files (x86)\Battle.net\Hearthstone\Hearthstone_Data\output_log.txt"
+                if (e.FullPath == path)
+                {
+                    string read = File.ReadAllLines(path).Last();
+                    if (read.Contains("OPPOSING"))
+                    {
+                        
+                    }
+                }
+            }
+            catch
+            {}
         }
     }
 }
